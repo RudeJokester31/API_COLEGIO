81567609a72b3fec241d37eaf0de7a1fb9bc293d
ejhostin0431 <jhostinepalza0411@gmail.com>
2023-02-06 19:42:20 -0500

    Primera version API colegio

---
+++ config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = ""
+    MYSQL_DB = "colegio"
+
+config = {
+    "development": DevelopmentConfig
+}
