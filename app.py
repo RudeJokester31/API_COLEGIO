5caa6bd6d04f4017102393113bde0c9ea69719dc
Bryan <bryan.31165@gmail.com>
2023-03-15 15:44:49 -0500

    grafico de ingresos

--- app.py
+++ app.py
@@ -3,11 +3,128 @@ from flask_mysqldb import MySQL
 from config import config
 from flask_cors import CORS
 import utils
+from datetime import datetime
 
 
 app = Flask(__name__)
 CORS(app)
 conexion = MySQL(app)
+
+
+@app.route("/ingresos")
+def get_ingresos():
+    try:
+        cursor = conexion.connection.cursor()
+        fecha_actual = datetime.now().date()
+        sql = "SELECT COUNT(FECHA) AS INGRESOS FROM ingresos WHERE DATE(FECHA) = '{0}' AND TIPO_INGRESO = 1".format(
+            fecha_actual)
+        print(fecha_actual)
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        return jsonify({"ingresos": data[0]})
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
+
+
+@app.route("/total_estudiantes")
+def get_total_estudiantes():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT COUNT(*) AS TOTAL_ESTUDIANTES FROM usuario WHERE ROL = 'estudiante'"
+        cursor.execute(sql)
+        count1 = cursor.fetchone()[0]
+        data1 = []
+        data1.append(count1)
+        print(data1)
+        return jsonify(data1)
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
+
+
+@app.route("/inasistencia")
+def get_inasistencia():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = """SELECT (SELECT COUNT(*) AS TOTAL_ESTUDIANTES FROM usuario WHERE ROL = 'estudiante') - (SELECT COUNT(FECHA) FROM ingresos WHERE DATE(FECHA) = CURDATE() AND TIPO_INGRESO = 1) AS INASISTENCIA"""
+        cursor.execute(sql)
+        count2 = cursor.fetchone()
+        data2 = []
+        data2.append(count2)
+        print(data2)
+        return jsonify({"inasistencias": count2})
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
+
+
+@app.route("/promedio")
+def get_promedio():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = """SELECT DATE_FORMAT(fecha, '%Y-%m') AS mes, AVG(cantidad_ingresos) AS PROMEDIO FROM (SELECT DATE(fecha) AS fecha, COUNT(*) AS cantidad_ingresos FROM ingresos GROUP BY DATE(fecha)) AS ingresos_por_fecha GROUP BY mes"""
+        cursor.execute(sql)
+        count3 = float(cursor.fetchone()[1])
+        data3 = []
+        data3.append(count3)
+        print(data3)
+        return jsonify(data3)
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
+
+
+@app.route("/estadisticas")
+def get_estadisticas():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = """SELECT DATE_FORMAT(FECHA, '%Y-%m-%d') AS FECHA, SUM(CANTIDAD_INGRESOS) AS TOTAL_INGRESOS FROM (
+        SELECT DATE(FECHA) AS FECHA, COUNT(*) AS CANTIDAD_INGRESOS FROM ingresos GROUP BY DATE(FECHA)) AS INGRESOS_POR_FECHA
+        GROUP BY DATE(FECHA)"""
+        cursor.execute(sql)
+        estadisticas = cursor.fetchall()
+        resultados = []
+        print(estadisticas)
+        for fecha, total_ingresos in estadisticas:
+            resultado = {"fecha": fecha, "total_ingresos": total_ingresos}
+            resultados.append(resultado)
+        return jsonify(resultados)
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
+
+
+@app.route("/l_ingresos", methods=['GET'])
+def listar_ingresos():
+    try:
+        # variable de cursor para la bd
+        cursor = conexion.connection.cursor()
+        sql = "SELECT * FROM ingresos ORDER BY ID_INGRESO"
+        cursor.execute(sql)
+        datos = cursor.fetchall()
+        ingresos = []
+        for fila in datos:
+            ingreso = {"ID_USUARIO": fila[1],
+                       "FECHA": fila[2], "ESTADO": fila[3]}
+            ingresos.append(ingreso)
+        return jsonify(ingresos)
+    except Exception as ex:
+        return jsonify({"mensaje": ex, "Codigo": False})
+
+
+@app.route("/estadisticas_grafico")
+def get_estadisticas_grafico():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = """SELECT DATE_FORMAT(FECHA, '%Y-%m-%d') AS FECHA, SUM(CANTIDAD_INGRESOS) AS TOTAL_INGRESOS FROM (
+        SELECT DATE(FECHA) AS FECHA, COUNT(*) AS CANTIDAD_INGRESOS FROM ingresos GROUP BY DATE(FECHA)) AS INGRESOS_POR_FECHA
+        GROUP BY DATE(FECHA) DESC LIMIT 5"""
+        cursor.execute(sql)
+        estadisticas = cursor.fetchall()
+        resultados = []
+        print(estadisticas)
+        for x in estadisticas:
+            resultado = {"fecha": x[0], "cantidad": x[1]}
+            resultados.append(resultado)
+        return jsonify(resultados)
+    except:
+        return jsonify({"mensaje": "No se completó la consulta", "Codigo": False})
 
 
 @app.route("/usuarios", methods=['GET'])
@@ -19,9 +136,10 @@ def listar_usuarios():
         cursor.execute(sql)
         datos = cursor.fetchall()
         usuarios = []
+        print(datos)
         for fila in datos:
-            usuario = {"id": fila[0], "username": fila[1], "password": fila[2], "NOMBRES": fila[3], "APELLIDOS": fila[4],
-                       "EDAD": fila[5], "GRADO": fila[6], "ROL": fila[7], "ID_HUELLA": fila[8]}
+            usuario = {"id": fila[0], "username": fila[1], "password": fila[2], "NOMBRES": fila[3],
+                       "APELLIDOS": fila[4], "EDAD": fila[5], "GRADO": fila[6], "ROL": fila[7], "ID_HUELLA": fila[8]}
             usuarios.append(usuario)
         return jsonify(usuarios)
     except Exception as ex:
@@ -31,7 +149,7 @@ def listar_usuarios():
 @app.route("/un_usuario/", methods=['POST'])
 def consultar_Un_Usuario(id):
     try:
-        one_User=request.json
+        one_User = request.json
         cursor = conexion.connection.cursor()
         sql = "SELECT * FROM usuario WHERE id='{0}'".format(id)
         cursor.execute(sql)
